afed86a7052165014a20368a850828f48ede0c08
Duncan Perkins <perkinsdt@goldmail.etsu.edu>
2016-09-24 15:44:42 -0400

    layout updates and inital configurations

---
+++ UPDWA/src/UPDWA/Controllers/AlertsController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace UPDWA.Controllers
+{
+    public class AlertsController : Controller
+    {
+        public IActionResult List()
+        {
+            return View();
+        }
+    }
+}
